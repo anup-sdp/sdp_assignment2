e4c70585745849f140c757550138eee2cbe119ca
anup30 <anupbarua30@gmail.com>
2025-04-13 01:48:52 +0600

    update

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ function showDrinksCards(){
 			</div>
 		`;
 		div.querySelector("#addToGroup-btn").addEventListener("click", ()=>{
-			addToGroup(e["idDrink"], e["strDrink"]);
+			//addToGroup(e["idDrink"], e["strDrink"]);
+			addToGroup(e);
 		});
 		div.querySelector("#details-btn").addEventListener("click", ()=>{
 			showDrinkDetails(e["idDrink"]);
@@ -35,7 +36,7 @@ function showDrinksCards(){
 function initialDrinks(){
 	//const defaultDrinksUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink";
 	//const defaultDrinksUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"; 
-	// www.thecocktaildb.com/api/json/v1/1/search.php?f=a
+	// www.thecocktaildb.com/api/json/v1/1/search.php?f=a // set as initial drinks
 	fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a").then(res=> res.json()).then(data=>{
 		//console.log(data);			
 		allDrinks = data["drinks"];
@@ -52,6 +53,75 @@ function initialDrinks(){
 }
 
 initialDrinks();
+
+
+function addToGroup(elem){
+	const drinkItem = cartGroup.find(item => item.idDrink == elem.idDrink);
+	if(drinkItem){
+		alert("item already exists in Group");
+		return;
+	}
+	else if(cartGroup.length>=7){
+		alert("can't add more than 7 items to Group");
+		return;
+	}
+	else{
+		cartGroup.push({...elem}); // ---
+		showDrinkGroup();
+	}
+}
+
+function showDrinkDetails(drinkId){
+	// search full cocktail details by id
+	const drinkUrl = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`; 
+	fetch(drinkUrl).then(res=> res.json()).then(data=>{
+		//console.log(data);			
+		let drink = data["drinks"];
+		console.log(drink);
+		if(data["drinks"]==null){
+			console.log("No drinks found by this id!");
+			//document.getElementById("searchResults").innerHTML="<p>No drinks found for this search!</p>";
+			return;
+		}	
+		showModal(drink[0]);
+	}).catch((err)=>{
+		console.log(err);
+	});
+}
+
+function showModal(drink){	
+	let modalBody = document.getElementById("modal-body");
+	modalBody.innerHTML = `
+		<img src="${drink.strDrinkThumb}" alt="drink-pic"> 
+		<p>Drink id: ${drink.idDrink}</p>
+		<p>name: ${drink.strDrink}</p>		
+		<p>category: ${drink.strCategory}</p>
+		<p>alcoholic: ${drink.strAlcoholic}</p>
+		<p>instructions: ${drink.strInstructions}</p>
+	`; // alternate format: drink["strInstructions"]
+	let myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+	myModal.show();	
+}
+
+function showDrinkGroup(){
+	let groupList = document.getElementById("group-list");
+	let groupListCount = document.getElementById("group-list-count");
+	groupList.innerHTML="";
+	cartGroup.forEach((e,idx)=>{
+		let div = document.createElement("div");
+		div.classList+=("card d-flex flex-row mb-1 p-1");
+		div.innerHTML= `
+			<span class="badge text-bg-secondary">${idx+1}</span>
+			<img src="${e["strDrinkThumb"]}" alt="image">
+			<div>
+				<p>name: ${e.strDrink}</p>
+				<p>id: ${e.idDrink}</p>
+			</div>
+		`;
+		groupList.appendChild(div);
+		groupListCount.innerHTML = `<h5>Group List: (${cartGroup.length})</h5>`;
+	});	
+}
 
 
 document.getElementById('searchButton').addEventListener('click', function(){
@@ -88,69 +158,3 @@ document.getElementById('searchButton').addEventListener('click', function(){
 		});
 	}	
 });
-
-function addToGroup(drinkId, drinkName){
-	const drinkItem = cartGroup.find(item => item.idDrink == drinkId);
-	if(drinkItem){
-		alert("item already exists in Group");
-		return;
-	}
-	else if(cartGroup.length>=7){
-		alert("can't add more than 7 items to Group");
-		return;
-	}
-	else{
-		cartGroup.push({idDrink:drinkId, name: drinkName}); // ---
-		showDrinkGroup();
-	}
-}
-
-function showDrinkDetails(drinkId){
-	// search full cocktail details by id // eg. www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007
-	const drinkUrl = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`; 
-	fetch(drinkUrl).then(res=> res.json()).then(data=>{
-		//console.log(data);			
-		let drink = data["drinks"];
-		console.log(drink);
-		if(data["drinks"]==null){
-			console.log("No drinks found by this id!");
-			//document.getElementById("searchResults").innerHTML="<p>No drinks found for this search!</p>";
-			return;
-		}	
-		showModal(drink[0]);
-	}).catch((err)=>{
-		console.log(err);
-	});
-}
-
-function showModal(drink){
-	//console.log("drink detail", drink.strDrink, drink["strDrink"]);
-	let modalBody = document.getElementById("modal-body");
-	modalBody.innerHTML = `
-		<!-- <img src="${drink.strImageSource}" alt="drink-pic"> problem -->
-		<p>id: ${drink.idDrink}</p>
-		<p>name: ${drink.strDrink}</p>		
-		<p>category: ${drink.strCategory}</p>
-		<p>alcoholic: ${drink.strAlcoholic}</p>
-		<p>instructions: ${drink.strInstructions}</p>
-	`; // alternate format: drink["strInstructions"]
-	let myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
-	myModal.show();	
-}
-
-function showDrinkGroup(){
-	let groupList = document.getElementById("group-list");
-	let groupListCount = document.getElementById("group-list-count");
-	groupList.innerHTML="";
-	cartGroup.forEach(e=>{
-		let div = document.createElement("div");
-		div.classList+=("card mb-1 p-1");
-		div.innerHTML= `
-			<p>name: ${e.name}</p>
-			<p>id: ${e.idDrink}</p>
-		`;
-		groupList.appendChild(div);
-		groupListCount.innerHTML = `<h3>Group List: (${cartGroup.length})</h3>`;
-	});	
-}
-
